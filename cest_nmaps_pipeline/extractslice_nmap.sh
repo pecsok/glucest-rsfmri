693d08ceefa9244aba16e9717dcc57807a78b40b
Margaret  Pecsok <mkpecsok@gmail.com>
2024-06-19 15:19:37 -0400

    add nmaps stuff

--- cest_nmaps_pipeline/extractslice_nmap.sh
+++ cest_nmaps_pipeline/extractslice_nmap.sh
@@ -2,21 +2,19 @@
 
 ## DEFINE PATHS ##
 structural=$1
-pre=$2
-post=$3
-atlas=$4
-log=$5
-case=$6
-method=$7
-reso=$8
-str=$9
+outpath=$2
+case=$3
+nmap=$4
 
 #######################################################################################################
 ## TRANSFORM NMAP TO SUBJECT SPACE ##
 
+
 antsApplyTransforms -d 3 -r $structural/$case/${case}-${str}_masked.nii.gz \
-  -i $atlas/Schaefer2018/Schaefer2018_100Parcels_17Networks_${reso}mm.nii.gz \
+  -i $nmap \
   -n MultiLabel \
-  -o $post/$case/atlases/$str/${case}-Schaefer2018-100P-17N.nii.gz \
+  -o $outpath/$case \
   -t [$structural/$case/MNI_transforms/${case}-${str}inMNI-0GenericAffine.mat,1] \
   -t $structural/$case/MNI_transforms/${case}-${str}inMNI-1InverseWarp.nii.gz
+
+  
